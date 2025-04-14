62b2228566c8edea350b9aa876a5c0e3bfc7d257
RodrigoLupo <rodrigo.lupo@tecsup.edu.pe>
2025-04-14 15:27:50 -0500

    primer

---
+++ Models/Profesore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LAB05_Lupo.Models;
+
+public partial class Profesore
+{
+    public int IdProfesor { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string? Especialidad { get; set; }
+
+    public string? Correo { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//builder.Services.AddDbContext<EscuelaDbContext>(options =>
+//    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.     
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+    c.RoutePrefix = string.Empty;
+});
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
